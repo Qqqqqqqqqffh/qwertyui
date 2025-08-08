3bcb4995b14739f0c19e3f3d94fbe56c8fe3fc66
qwertyucvw <byziaeoph@gmail.com>
2025-08-09 01:46:56 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,13 +3,13 @@
 #include <algorithm>
 #include <chrono>
 #include <fstream>
+
+#include <iomanip>
+#include <cstring>
 #include <random>
 #include <sstream>
 #include <filesystem>
 #include <cctype>
-#include <iomanip>
-#include <cstring>
-
 namespace fs = std::filesystem;
 
 std::string generate_json(const std::string& username, double gen_time, double sort_time, bool sorted) {
